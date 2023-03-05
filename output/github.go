aeca5d9c59e9766a995c085d81386a7ceb68d806
Sergey Vilgelm <sergey@vilgelm.com>
2023-03-05 13:13:25 -0800

    move output to dedicated package

---
+++ output/github.go
@@ -0,0 +1,77 @@
+package output
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+func PrintAsGithub(diag *Diagnostic) {
+	// print console output for people
+	os.Stdout.WriteString("::group::console format\n")
+	PrintAsConsole(diag)
+	os.Stdout.WriteString("::endgroup::\n")
+
+	wg := sync.WaitGroup{}
+	for _, pkg := range *diag {
+		for name, issues := range pkg {
+			for _, issue := range issues {
+				wg.Add(1)
+				name := name
+				issue := issue
+				go func() {
+					defer wg.Done()
+					filename, line, pos := parsePosN(issue.PosN)
+
+					f, err := getFile(filename)
+					if err != nil {
+						log.Printf("reading file %q failed: %+v", filename, err)
+						return
+					}
+
+					buf := bytes.Buffer{}
+					buf.WriteString("::error file=")
+					buf.WriteString(f.Filename)
+					if line != -1 {
+						buf.WriteString(",line=")
+						buf.WriteString(strconv.Itoa(line))
+						if pos != -1 {
+							buf.WriteString(",col=")
+							buf.WriteString(strconv.Itoa(pos))
+						}
+					}
+					buf.WriteString("::")
+					if issue.Category != "" {
+						buf.WriteString(issue.Category)
+						buf.WriteString(": ")
+					}
+					if issue.Message != "" {
+						buf.WriteString(issue.Message)
+					}
+					buf.WriteString(" (")
+					buf.WriteString(name)
+					buf.WriteRune(')')
+					if line != -1 && line < len(f.Lines) {
+						buf.WriteString("%0A")
+						buf.WriteString(strings.Replace(f.Lines[line-1], "\t", " ", pos))
+						if pos != -1 {
+							buf.WriteString("%0A")
+							buf.Grow(pos)
+							for i := 0; i < pos-1; i++ {
+								buf.WriteRune(' ')
+							}
+							buf.WriteRune('^')
+						}
+					}
+
+					buf.WriteRune('\n')
+					buf.WriteTo(os.Stdout)
+				}()
+			}
+		}
+	}
+	wg.Wait()
+}
---
+++ runner.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/sv-tools/gochecker/config"
+	"github.com/sv-tools/gochecker/output"
+)
+
+func intercept() {
+	conf := config.ParseConfig()
+
+	buf := runMultiChecker(conf.Args...)
+	// exit if no issues
+	if buf.Len() == 3 && bytes.Equal(bytes.TrimSpace(buf.Bytes()), []byte("{}")) {
+		return
+	}
+	diag := output.ParseOutput(buf)
+	output.Exclude(conf, diag)
+	if len(*diag) == 0 {
+		os.Exit(0)
+	}
+	switch conf.Output {
+	case config.ConsoleOutput:
+		output.PrintAsConsole(diag)
+		os.Exit(3)
+	case config.JSONOutput:
+		e := json.NewEncoder(os.Stdout)
+		e.SetIndent("", "  ")
+		if err := e.Encode(diag); err != nil {
+			log.Fatalf("json ouput failed: %+v", err)
+		}
+	case config.GithubOutput:
+		output.PrintAsGithub(diag)
+		os.Exit(3)
+	}
+}
+
+func runMultiChecker(args ...string) *bytes.Buffer {
+	var (
+		stderr bytes.Buffer
+		stdout bytes.Buffer
+	)
+	prog, err := os.Executable()
+	if err != nil {
+		log.Fatalf("getting execuable failed: %+v", err)
+	}
+	prog, err = filepath.EvalSymlinks(prog)
+	if err != nil {
+		log.Fatalf("evaluating symlink failed: %+v", err)
+	}
+
+	cmd := exec.Command(prog, args...)
+	cmd.Env = append(os.Environ(), interceptModeEnv+"=on")
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err = cmd.Run()
+	var code = -1
+	if cmd.ProcessState != nil {
+		code = cmd.ProcessState.ExitCode()
+	}
+	if stderr.Len() > 0 {
+		stderr.WriteTo(os.Stderr)
+		os.Exit(code)
+	}
+	if err != nil {
+		log.Fatalf("interception failed: %+v", err)
+	}
+	return &stdout
+}
